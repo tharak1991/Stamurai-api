5a05aae24785441df97be80d31984fc5fcb75868
tharak1991 <tharak1991@gmail.com>
2020-02-28 21:09:56 +0530

    completed

--- controllers/word_cntrl.js
+++ controllers/word_cntrl.js
@@ -2,74 +2,7 @@ var Word  =  require('../models/word');
 
 module.exports = {
 
-     getCashback: async function (req, res) {
-
-        try {
-
-            var query = {start : 'A'}
-
-        if (true) {
-            Word.findOne(query, function (err, cashback) {
-                if (err) throw err;
-                if (!cashback) res.status(200).send({
-                    success: false,
-                    message: '12'
-                });
-                else
-                    res.status(200).send({
-                        success: true,
-                        message: cashback
-                    });
-            }).select({
-                "_meta": 0,
-                "_id": 0,
-                "__v": 0
-            });
-        } else {
-            res.status(401).send({
-                sucess: false,
-                message: "Token Invalid"
-            });
-        }
-    }catch (err) {
-        next(err);
-      }
-    },
-
-    getCashback3: async function (req, res) {
-
-        try {
-
-            var query = {start : 'A'}
-
-        if (true) {
-            Word.find(query, function (err, cashback) {
-                if (err) throw err;
-                if (!cashback) res.status(200).send({
-                    success: false,
-                    message: '12'
-                });
-                else
-                    res.status(200).send({
-                        success: true,
-                        message: cashback
-                    });
-            }).limit(100).select({
-                "_meta": 0,
-                "_id": 0,
-                "__v": 0
-            });
-        } else {
-            res.status(401).send({
-                sucess: false,
-                message: "Token Invalid"
-            });
-        }
-    }catch (err) {
-        next(err);
-      }
-    },
-
+     
     
     getCashback4: async function (req, res) {
 
@@ -108,54 +41,31 @@ module.exports = {
       }
     },
 
-    addWord: function (req, res) {
-
-        var newWord = new Word({
-            2: 'at'
-        });
-        newWord.save()
-            .then(item => {
-                res.send("item saved to database");
-            })
-            .catch(err => {
-                res.status(400).send("unable to save to database");
-            });
-    },
-
-
-
-    getCashback2: async function (req, res) {
+    getCashback5: async function (req, res) {
 
         try {
 
-            var results = [];
+            var search = req.body.words;
 
-            var query = {start : ['A']};
+            // var search = [];
 
         if (true) {
-            var cursor = Word.find(query).limit(4);
-            if(true){
-                res.status(200).send({
-                    success: true,
-                    message: cursor
+            Word.find( { start: { $regex: '' + search[0] + '.*' + '|' + search[1] + '.*'  } } , function (err, result) {
+                if (err) throw err;
+                if (!result) res.status(200).send({
+                    success: false,
+                    message: 'No match!!!'
                 });
-
-            }
-            // Word.find(query).limit(3).forEach(function (doc) {
-            //     if (!cursor) res.status(200).send({
-            //         success: false,
-            //         message: '12'
-            //     });
-            //     else
-            //         res.status(200).send({
-            //             success: true,
-            //             message: cursor
-            //         });
-            // }).select({
-            //     "_meta": 0,
-            //     "_id": 0,
-            //     "__v": 0
-            // });
+                else
+                    res.status(200).send({
+                        success: true,
+                        message: result
+                    });
+            }).limit(400).select({
+                "_meta": 0,
+                "_id": 0,
+                "__v": 0
+            });
         } else {
             res.status(401).send({
                 sucess: false,
@@ -166,6 +76,8 @@ module.exports = {
         next(err);
       }
     }
+
+    
     
 }
 
--- routes/stamurai.js
+++ routes/stamurai.js
@@ -5,11 +5,8 @@ var router = express.Router();
 
 var word_cntrl = require('../controllers/word_cntrl');
 
-router.get('/get', word_cntrl.getCashback);
-router.post('/add', word_cntrl.addWord);
 
-router.get('/get2', word_cntrl.getCashback2);
-router.get('/get3', word_cntrl.getCashback3);
 router.get('/get4', word_cntrl.getCashback4);
+router.post('/get5', word_cntrl.getCashback5);
 
 module.exports = router ;
